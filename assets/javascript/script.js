26d55a45b11ddeb7bbaa78371ea6bb4e3d20492d
Dark Knight <jordanccall@gmail.com>
2020-05-02 14:45:06 -0600

    updated the live page with current portfolio

---
+++ assets/javascript/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+
+  $('.modal').modal();
+
+  $('.parallax').parallax();
+
+
+  $('.carousel').carousel({
+    indicators: true,
+  });
+
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCTlCEPrbZ3EBwGJeUyVfkI3p54fJP8Bec",
+    authDomain: "jccall-portfolio.firebaseapp.com",
+    databaseURL: "https://jccall-portfolio.firebaseio.com",
+    projectId: "jccall-portfolio",
+    storageBucket: "jccall-portfolio.appspot.com",
+    messagingSenderId: "879674023834",
+    appId: "1:879674023834:web:fbe3e76f046e47c99ae423"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var newContact = [];
+  var database = firebase.database();
+
+  $("#submitThisForm").on("click", function () {
+    newContact.firstName = $("#name").val();
+    newContact.email = $("#email").val();
+    newContact.message = $("#messageinput").val();
+
+    database.ref().push(newContact);
+    document.getElementById("myForm").reset();
+  });
+
+
+
+
+});
